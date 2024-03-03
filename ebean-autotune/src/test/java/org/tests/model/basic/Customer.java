cc51208337ae84680d0703f742ecf1975de10321
Rob Bygrave <robin.bygrave@gmail.com>
2024-03-03 16:00:29 +1300

    Update for ebean 14.x

--- ebean-autotune/src/test/java/org/tests/model/basic/Customer.java
+++ ebean-autotune/src/test/java/org/tests/model/basic/Customer.java
@@ -2,10 +2,10 @@ package org.tests.model.basic;
 
 import io.ebean.annotation.NotNull;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "o_customer")
